ff7c2d1143be86de13cb621befe1787457d19969
IsraelBarbosa <45472540+IsraelBarbosa@users.noreply.github.com>
2020-02-11 10:20:31 -0300

    Update index.php

--- index.php
+++ index.php
@@ -11,23 +11,23 @@
     <title>Landing Page-Israel Barbosa</title>
 </head>
 
-<body class="bg-info">
+<body>
     <main class="container col-lg-12 col-md-12 col-sm-12 col-xs-12">
         <section class="row">
             <section class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
                 <h1><b>Israel Barbosa</b></h1>
                 <h2 class="subTitulo"><b>Desenvolvedor Front-end</b></h2>
                 <div class="apresentacao">
-                    <h3 class="text-success tituloDaApresentacao"> - Quem sou eu</h3>
-                    <p class="descApresentacao">Olá, meu nome é Israel Barbosa, sou estudante do curso tecnólogo em
-                        sistemas de internet, na faculdade São Miguel.</p>
-                    <h3 class="text-success tituloDaApresentacao"> - Objetivo</h3>
-                    <p class="descApresentacao">Estágio como desenvolvedor Front-end.</p>
-                    <h3 class="text-success tituloDaApresentacao"> - Habilidades</h3>
-                    <p class="descApresentacao">Tenho uma base em HTML, CSS e noções em bootstrap e, pretendo adquirir
-                        mais conhecimentos
-                        daqui pra frente me aprofundado cada vez mais. Também iniciei os meus estudos em JavaScript,
-                        e possuo noções de desenvolvimento de programas básicos.
+                    <p class="descApresentacao">Olá, meu nome é Israel Barbosa, tenho vinte e um anos, sou estudante do
+                        quinto período do curso Sistemas de Internet da faculdade São Miguel.
+                    </p>
+                    <p class="descApresentacao">Estou em busca de uma oportunidade como estagiário desenvolvedor
+                        Front-end; quero trabalhar com vocês da Audax, para aprofundar meus conhecimentos e adquirir
+                        novas experiências.</p>
+                    <p class="descApresentacao">Tenho uma base em HTML, CSS e noções em bootstrap. Também iniciei os
+                        meus estudos em JavaScript, e possuo noções de desenvolvimento de programas básicos.</p>
+                    <p class="descApresentacao">Pretendo estar sempre atualizando meus conhecimentos através de cursos.
+                        E também fazer uma pós graduação para ser mais capacitado no meu trabalho.
                     </p>
                 </div>
             </section>
@@ -49,8 +49,7 @@
 
     <script src="script.js"></script>
     <?php 
-    $var = "ola mundo";
-    ?>
+$var = "ola mundo";
+?>
 </body>
-
 </html>
